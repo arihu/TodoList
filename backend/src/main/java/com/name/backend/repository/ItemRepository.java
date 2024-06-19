41cae05e0b78b2ce8e938bbbf226427ba26355f8
Ari Hu <ariwho1@gmail.com>
2024-06-18 23:29:53 -0500

    Finished API

--- backend/src/main/java/com/name/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/name/backend/repository/ItemRepository.java
@@ -1,4 +1,7 @@
 package com.name.backend.repository;
 
-public class ItemRepository {
+import com.name.backend.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
 }
